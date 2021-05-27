bc9be9080f052683eb142ea9b9d210adee045e67
Ever-coder <83790918+Ever-coder@users.noreply.github.com>
2021-05-27 08:59:30 -0600

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -4,13 +4,20 @@ $mail = $_POST['mail'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 
-$destinatario = "ever_franco7@hotmail.com";
-$asunto = "prueba";
+$header = 'From: ' . $mail . " \r\n";
+$header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
+$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Content-Type: text/plain";
 
-$carta = "De: $name \n";
-$carta .= "Mail: $mail";
+$message = "Este mensaje fue enviado por: " . $name . " \r\n";
+$message .= "Su e-mail es: " . $mail . " \r\n";
+$message .= "Teléfono de contacto: " . $phone . " \r\n";
+$message .= "Mensaje: " . $_POST['message'] . " \r\n";
+$message .= "Enviado el: " . date('d/m/Y', time());
 
-mail($destinatario, $asunto, $carta);
+$para = 'milenioflavio@hotmail.com';
+$asunto = 'Mensaje de PRECOMPC';
+
+mail($para, $asunto, utf8_decode($message), $header);
 
 header("Location:index.html");
-?>
